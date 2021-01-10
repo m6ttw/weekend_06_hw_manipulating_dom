b77b08ee5c7393c3325ba660b857f043185c0e4f
m6ttw <matthewrhyswilliams@live.com>
2021-01-10 16:12:08 +0000

    formatting of list entries

--- js/app.js
+++ js/app.js
@@ -9,16 +9,38 @@ document.addEventListener("DOMContentLoaded", () => {
 const handleFormSubmit = function (event) {
 	event.preventDefault();
 	
-	const newListItem = document.createElement("li");
-	newListItem.textContent = `${this.title.value} ${this.year.value} ${this.genre.value} ${this.director.value} ${this.rating.value}`;
+    const movieListItem = createMovieListItem(event.target);
   
-	const list = document.querySelector("#movies-list");
-	list.appendChild(newListItem);
+	const movieList = document.querySelector("#movie-list");
+	movieList.appendChild(movieListItem);
 	
 	document.querySelector("#new-movie-form").reset();
 };
 
+const createMovieListItem = function (form) {
+    const MovieListItem = document.createElement('li');
+    MovieListItem.classList.add('movie-list-item');
+  
+    const titleAndYear = document.createElement('h3');
+    titleAndYear.textContent = `${form.title.value} (${form.year.value})`;
+    MovieListItem.appendChild(titleAndYear);
+  
+    const genre = document.createElement('p');
+    genre.textContent = form.genre.value;
+    MovieListItem.appendChild(genre);
+  
+    const director = document.createElement('p');
+    director.textContent = `Director: ${form.director.value}`;
+    MovieListItem.appendChild(director);
+  
+    const rating = document.createElement('p');
+    rating.textContent = `Rating: ${form.rating.value}/10`;
+    MovieListItem.appendChild(rating);
+  
+    return MovieListItem;
+};
+
 const handleDeleteAllClick = function (event) {
-	const movieList = document.querySelector("#movies-list");
+	const movieList = document.querySelector("#movie-list");
 	movieList.innerHTML = "";
 }
